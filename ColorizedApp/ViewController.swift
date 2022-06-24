25bcc1b2489a95bfeef4dad6d8b9721d5f8d0c9f
Amuzey <ivanovad97@gmail.com>
2022-06-24 15:05:27 +0500

    Setup UI

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paletteColorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        paletteColorView.layer.cornerRadius = 15
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
     }
 
 
